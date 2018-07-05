d240288bb9ba8a151238126ef77ce701abb1c394
Hfragoso <heberfm@hotmail.com>
2018-07-04 23:44:18 -0700

    First commit

---
+++ app/src/main/java/com/example/heber/singletonrotationtest/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.heber.singletonrotationtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView dateTextView = findViewById(R.id.tvDate);
+        MyExample myExample = MyExample.getInstance();
+        dateTextView.setText((new Date(myExample.getDate())).toString());
+    }
+}
---
+++ app/src/main/java/com/example/heber/singletonrotationtest/MyExample.java
@@ -0,0 +1,24 @@
+package com.example.heber.singletonrotationtest;
+
+import java.util.Date;
+
+final class MyExample {
+    private static MyExample instance = null;
+    private long mDate;
+
+    static MyExample getInstance() {
+        if (instance == null) {
+            instance = new MyExample();
+        }
+
+        return instance;
+    }
+
+    private MyExample(){
+        mDate = new Date().getTime();
+    }
+
+    long getDate(){
+        return mDate;
+    }
+}
